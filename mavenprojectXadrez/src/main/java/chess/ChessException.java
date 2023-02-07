5beb77f0770b484af37fb13e591a31d00aaefd01
Leandro Ferreira <leanfj@hotmail.com>
2023-02-06 22:46:16 -0300

    Xadrez - ChessException e chessposition

---
+++ mavenprojectXadrez/src/main/java/chess/ChessException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess;
+
+/**
+ *
+ * @author leandro
+ */
+public class ChessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+    
+    
+}
--- mavenprojectXadrez/src/main/java/chess/ChessMatch.java
+++ mavenprojectXadrez/src/main/java/chess/ChessMatch.java
@@ -5,7 +5,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -14,28 +13,33 @@ import chess.pieces.Rook;
  * @author leandro
  */
 public class ChessMatch {
-
+    
     private Board board;
-
+    
     public ChessMatch() {
         this.board = new Board(8, 8);
         this.initialSetup();
     }
-
+    
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-
+        
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
-
+        
         return mat;
     }
-
+    
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+    
     private void initialSetup() {
-        board.placePiece(new Rook(this.board, Color.BLACK, 'R'), new Position(2, 1));
-        board.placePiece(new King(this.board, Color.WITHE, 'K'), new Position(0, 4));
+        placeNewPiece('b', 6, new Rook(this.board, Color.BLACK, 'R'));
+        placeNewPiece('e', 1, new King(this.board, Color.WITHE, 'K'));
+        
     }
 }
