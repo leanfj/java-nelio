5c3b0c297454e5fc76360c3959a99742d6a14832
Leandro Ferreira <leanfj@hotmail.com>
2023-02-07 23:33:00 -0300

    Xadrez - Movimentando peças

--- mavenprojectXadrez/src/main/java/application/MavenprojectXadrez.java
+++ mavenprojectXadrez/src/main/java/application/MavenprojectXadrez.java
@@ -2,10 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +17,20 @@ public class MavenprojectXadrez {
 
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        
-        UI.printBoard(chessMatch.getPieces());
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(scanner);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(scanner);
+            
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+        }
     }
 }
--- mavenprojectXadrez/src/main/java/chess/ChessException.java
+++ mavenprojectXadrez/src/main/java/chess/ChessException.java
@@ -4,11 +4,13 @@
  */
 package chess;
 
+import boardgame.BoardException;
+
 /**
  *
  * @author leandro
  */
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
     private static final long serialVersionUID = 1L;
 
     public ChessException(String msg) {
