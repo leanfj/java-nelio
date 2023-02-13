6c3fcc5a6f3c806d59b371f81f5f73daaee939c6
Leandro Ferreira <leanfj@hotmail.com>
2023-02-13 18:49:52 -0300

    Xadrez - Implementando movimentos do rei

--- mavenprojectXadrez/src/main/java/chess/pieces/King.java
+++ mavenprojectXadrez/src/main/java/chess/pieces/King.java
@@ -5,6 +5,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -12,7 +13,7 @@ import chess.Color;
  *
  * @author leandro
  */
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
     public King(Board board, Color color, char code) {
         super(color, board, code);
@@ -21,7 +22,74 @@ public class King extends ChessPiece{
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // above 
+        p.setValues(this.position.getRow() - 1, position.getColumn());
+
+        if (this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below
+        p.setValues(this.position.getRow() + 1, position.getColumn());
+
+        if (this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        
+        // left
+        p.setValues(this.position.getRow(), position.getColumn() - 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        
+        // rigth
+        p.setValues(this.position.getRow(), position.getColumn() + 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        //nw
+        p.setValues(this.position.getRow() - 1, position.getColumn() - 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        //ne
+        p.setValues(this.position.getRow() - 1, position.getColumn() + 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        //se
+        p.setValues(this.position.getRow() + 1, position.getColumn() + 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        
+        //sw
+        p.setValues(this.position.getRow() + 1, position.getColumn() - 1);
+        
+        if(this.getBoard().positionExists(p) && this.canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
         return mat;
     }
-    
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) this.getBoard().piece(position);
+
+        return p == null || p.getColor() != this.getColor();
+    }
+
 }
