0b25d5aa100d837f9ec032b943ba97968dd412ac
Leandro Ferreira <leanfj@hotmail.com>
2023-02-15 23:10:34 -0300

    Correção de retorno de teste de checkMate

--- mavenprojectXadrez/src/main/java/chess/ChessMatch.java
+++ mavenprojectXadrez/src/main/java/chess/ChessMatch.java
@@ -63,32 +63,32 @@ public class ChessMatch {
     public boolean[][] possibleMoves(ChessPosition sourcePosition) {
         Position position = sourcePosition.toPosition();
         this.validateSourcePosition(position);
-        return board.piece(position).possibleMoves();
+        return this.board.piece(position).possibleMoves();
 
     }
 
     public boolean getCheckMate() {
         return this.checkMate;
     }
-    
+
     private boolean testCheckMate(Color color) {
-        if(!testCheck(color)) {
+        if (!testCheck(color)) {
             return false;
         }
-        
-        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
-        for(Piece p : list) {
+
+        List<Piece> list = this.piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color).collect(Collectors.toList());
+        for (Piece p : list) {
             boolean[][] mat = p.possibleMoves();
-            for(int i = 0; i<board.getRows(); i++){
-                for(int j = 0; j<board.getColumns();j ++) {
-                    if(mat[i][j]) {
-                        Position source = ((ChessPiece)p).getChessPosition().toPosition();
+            for (int i = 0; i < this.board.getRows(); i++) {
+                for (int j = 0; j < this.board.getColumns(); j++) {
+                    if (mat[i][j]) {
+                        Position source = ((ChessPiece) p).getChessPosition().toPosition();
                         Position target = new Position(i, j);
-                        Piece capturedPiece = makeMove(source, target);
-                        boolean testCheck = testCheck(color);
-                        
-                        undoMove(source, target, capturedPiece);
-                        if(!testCheck) {
+                        Piece capturedPiece = this.makeMove(source, target);
+                        boolean testCheck = this.testCheck(color);
+
+                        this.undoMove(source, target, capturedPiece);
+                        if (!testCheck) {
                             return false;
                         }
                     }
@@ -103,51 +103,53 @@ public class ChessMatch {
         Position target = targetPosition.toPosition();
         this.validateSourcePosition(source);
         this.validateTargetPosition(source, target);
-        Piece capturedPiece = makeMove(source, target);
-        if (testCheck(currentPlayer)) {
+        Piece capturedPiece = this.makeMove(source, target);
+        if (this.testCheck(this.currentPlayer)) {
             this.undoMove(source, target, capturedPiece);
             throw new ChessException("You can't put yourself in check");
         }
 
-        check = (testCheck(opponent(currentPlayer))) ? true : false;
+        this.check = (this.testCheck(this.opponent(this.currentPlayer))) ? true : false;
 
-        if(testCheckMate(opponent(currentPlayer))){
-            checkMate = true;
+        if (this.testCheckMate(this.opponent(this.currentPlayer))) {
+            this.checkMate = true;
+        } else {
+            this.nextTurn();
+
         }
-        
-        this.nextTurn();
+
         return (ChessPiece) capturedPiece;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         this.board.placePiece(piece, new ChessPosition(column, row).toPosition());
-        piecesOnTheBoard.add(piece);
+        this.piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
-        this.placeNewPiece('c', 1, new Rook(board, Color.WHITE, 'R'));
-        this.placeNewPiece('c', 2, new Rook(board, Color.WHITE, 'R'));
-        this.placeNewPiece('d', 2, new Rook(board, Color.WHITE, 'R'));
-        this.placeNewPiece('e', 2, new Rook(board, Color.WHITE, 'R'));
-        this.placeNewPiece('e', 1, new Rook(board, Color.WHITE, 'R'));
-        this.placeNewPiece('d', 1, new King(board, Color.WHITE, 'K'));
+        this.placeNewPiece('c', 1, new Rook(this.board, Color.WHITE, 'R'));
+        this.placeNewPiece('c', 2, new Rook(this.board, Color.WHITE, 'R'));
+        this.placeNewPiece('d', 2, new Rook(this.board, Color.WHITE, 'R'));
+        this.placeNewPiece('e', 2, new Rook(this.board, Color.WHITE, 'R'));
+        this.placeNewPiece('e', 1, new Rook(this.board, Color.WHITE, 'R'));
+        this.placeNewPiece('d', 1, new King(this.board, Color.WHITE, 'K'));
 
-        this.placeNewPiece('c', 7, new Rook(board, Color.BLACK, 'R'));
-        this.placeNewPiece('c', 8, new Rook(board, Color.BLACK, 'R'));
-        this.placeNewPiece('d', 7, new Rook(board, Color.BLACK, 'R'));
-        this.placeNewPiece('e', 7, new Rook(board, Color.BLACK, 'R'));
-        this.placeNewPiece('e', 8, new Rook(board, Color.BLACK, 'R'));
-        this.placeNewPiece('d', 8, new King(board, Color.BLACK, 'K'));
+        this.placeNewPiece('c', 7, new Rook(this.board, Color.BLACK, 'R'));
+        this.placeNewPiece('c', 8, new Rook(this.board, Color.BLACK, 'R'));
+        this.placeNewPiece('d', 7, new Rook(this.board, Color.BLACK, 'R'));
+        this.placeNewPiece('e', 7, new Rook(this.board, Color.BLACK, 'R'));
+        this.placeNewPiece('e', 8, new Rook(this.board, Color.BLACK, 'R'));
+        this.placeNewPiece('d', 8, new King(this.board, Color.BLACK, 'K'));
     }
 
     private void validateSourcePosition(Position position) {
-        if (!board.thereIsAPiece(position)) {
+        if (!this.board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
         if (this.currentPlayer != ((ChessPiece) this.board.piece(position)).getColor()) {
             throw new ChessException("The chosen piece is not yours");
         }
-        if (!board.piece(position).isTherAnyPossibleMove()) {
+        if (!this.board.piece(position).isTherAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece!");
         }
     }
@@ -184,8 +186,8 @@ public class ChessMatch {
 
         if (capturedPiece != null) {
             this.board.placePiece(capturedPiece, target);
-            capturedPieces.remove(capturedPiece);
-            piecesOnTheBoard.add(capturedPiece);
+            this.capturedPieces.remove(capturedPiece);
+            this.piecesOnTheBoard.add(capturedPiece);
         }
     }
 
